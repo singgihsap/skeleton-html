1d256cef8fb1521dcb2f9ae6d83ea7547a4d603e
Singgih Saputro <singgihsap@gmail.com>
2019-02-01 15:57:35 +0700

    Update new skeleton

--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,12 @@ var browserSync = require('browser-sync').create();
 
 // target source scss
 var input = {
-    'sass': 'src/stylesheets/**/*.scss'
+  'sass': 'src/stylesheets/**/*.scss'
 };
 
 // output css
 var output = {
-    'stylesheets': 'src/assets/css'
+  'stylesheets': 'src/assets/css'
 };
 
 // run the watch task when gulp is called without arguments
@@ -21,47 +21,48 @@ gulp.task('default', ['watch']);
 
 // compile SCSS to CSS files
 gulp.task('build-css', function () {
-    return gulp.src('src/stylesheets/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(sourcemaps.init())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(sass())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(output.stylesheets))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
+  return gulp.src('src/stylesheets/**/*.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions'],
+    cascade: false
+  }))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest(output.stylesheets))
+  .pipe(browserSync.reload({
+    stream: true
+  }))
 });
 
 // for build dist
 gulp.task("build-dist", ['build-css'], function () {
-    return gulp.src(['src/*.html', "src/assets/css/**", "src/assets/images/**", "src/assets/fonts/**", "src/assets/js/**"], {base: './src'})
-        .pipe(gulp.dest('dist'));
+  return gulp.src(['src/*.html', "src/assets/css/**", "src/assets/images/**", "src/assets/fonts/**", "src/assets/js/**"], {base: './src'})
+  .pipe(gulp.dest('dist'));
 });
 
-// clean CSS
-gulp.task('clean-css', function () {
-    return gulp.src('src/assets/css/*.css')
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest(output.stylesheets));
+// Task to minify css using package cleanCSs
+gulp.task('minify-css', () => {
+  // Folder with files to minify
+  return gulp.src('src/assets/css/*.css')
+  .pipe(cleanCSS())
+  //I define the destination of the minified files with the method dest
+  .pipe(gulp.dest(output.stylesheets));
 });
 
 // BrowserSync
 gulp.task('browserSync', function () {
-    browserSync.init(['src/*.html', '*.html'], {
-        server: {
-            baseDir: './'
-        }
-    });
+  browserSync.init(['src/*.html', '*.html'], {
+    server: {
+      baseDir: './'
+    }
+  });
 });
 
 /* Watch these files for changes and run the task on update */
 gulp.task('watch', ['browserSync'], function () {
-    gulp.watch(input.sass, ['build-css']);
-    gulp.watch('./*.html', browserSync.reload);
-    gulp.watch('*.html').on('change', browserSync.reload);
-    gulp.watch(['./src/*.html', '*.php']).on('change', browserSync.reload);
+  gulp.watch(input.sass, ['build-css']);
+  gulp.watch('./*.html', browserSync.reload);
+  gulp.watch('*.html').on('change', browserSync.reload);
+  gulp.watch(['./src/*.html', '*.php']).on('change', browserSync.reload);
 });
